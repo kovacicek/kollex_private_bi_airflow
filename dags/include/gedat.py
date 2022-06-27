b07809908cd79a16ebbb2a7f3147716c5d049096
TheDataJanitor <mohamedaligalall@gmail.com>
2022-06-27 08:57:52 +0200

    BI-67

--- dags/include/gedat.py
+++ dags/include/gedat.py
@@ -27,83 +27,83 @@ def run_gedat():
     ############ with gedat
 
     query = """
-    with merchants as (
-            select '01'                                as "Satzart"
-                , '00000001'                          as "Empfänger"
-                , '43999023'                          as "Absender"
-                , '01'                                as "Versionsnummer"
-                , concat( 'M_', id_merchant )         as "Hersteller-Kunden-Nr."
-                , NULL                                as "GEDAT-Adressnummer"
-                , NULL                                as "GLN"
-                , NULL                                as "Leerfeld1"
-                , NULL                                as "Leerfeld2"
-                , NULL                                as "Gemeindekennziffer"
-                , 'Getränkefachgroßhandel'            as "Geschäftstyp"
-                , merchant.name                       as "Name-1 (Bezeichnung)"
-                , NULL                                as "Name-2 (Inhaber)"
-                , merchant.name                       as "Kurzbezeichnung"
-                , street                              as "Straße u. Hausnummer"
-                , zip                                 as "Postleitzahl"
-                , city                                as "Ort"
-                , 'Deutschland'                       as "Land"
-                , contact.mobile_phone                as "Telefon-1"
-                , NULL                                as "Telefon-2"
-                , NULL                                as "Leerfeld2"
-                , NULL                                as "Telefax"
-                , NULL                                as "Leerfeld3"
-                , to_char( now( )::date, 'YYYYMMDD' ) as "Übertragungsdatum"
-                , 'N'                                 as "Status"
-                , '10000004'                          as "Übertragungsnummer"
-            from fdw_customer_service.merchant
-                    left join fdw_customer_service.merchant_has_contacts
-                                on merchant.id_merchant = merchant_has_contacts.fk_merchant
-                    left join fdw_customer_service.contact on merchant_has_contacts.fk_contact
-                = contact.id_contact
-                    left join fdw_customer_service.address on address.id_address = merchant.fk_address
-
-        )
-
-
-    , customers as (
-                    select '01'                                as "Satzart"
-                        , '00000001'                          as "Empfänger"
-                        , '43999023'                          as "Absender"
-                        , '01'                                as "Versionsnummer"
-                        , concat( 'O_', id_customer )         as "Hersteller-Kunden-Nr."
-                        , NULL                                as "GEDAT-Adressnummer"
-                        , NULL                                as "GLN"
-                        , NULL                                as "Leerfeld"
-                        , NULL                                as "Leerfeld1"
-                        , NULL                                as "Gemeindekennziffer"
-                        , NULL                                as "Geschäftstyp"
-                        , customer.name                       as "Name-1 (Bezeichnung)"
-                        , customer.name2                      as "Name-2 (Inhaber)"
-                        , customer.short_name                 as "Kurzbezeichnung"
-                        , address.street                      as "Straße u. Hausnummer"
-                        , address.zip                         as "Postleitzahl"
-                        , address.city                        as "Ort"
-                        , address.country                     as "Land"
-                        , contact.mobile_phone                as "Telefon-1"
-                        , NULL                                as "Telefon-2"
-                        , NULL                                as "Leerfeld2"
-                        , NULL                                as "Telefax"
-                        , NULL                                as "Leerfeld3"
-                        , to_char( now( )::date, 'YYYYMMDD' ) as "Übertragungsdatum"
-                        , 'N'                                 as "Status"
-                        , '10000004'                          as "Übertragungsnummer"
-                    from fdw_customer_service.customer
-                            left join fdw_customer_service.address
-                                        on customer.fk_address = address.id_address
-                            left join fdw_customer_service.customer_has_contacts
-                                        on customer.id_customer = customer_has_contacts.fk_customer
-                            left join fdw_customer_service.contact
-                                        on customer_has_contacts.fk_contact = contact.id_contact
+            with merchants as (
+                            select '01'                                                as "Satzart"
+                                , '00000001'                                          as "Empfänger"
+                                , '43999023'                                          as "Absender"
+                                , '01'                                                as "Versionsnummer"
+                                , concat( 'M_', id_merchant )                         as "Hersteller-Kunden-Nr."
+                                , NULL                                                as "GEDAT-Adressnummer"
+                                , NULL                                                as "GLN"
+                                , NULL                                                as "Leerfeld1"
+                                , NULL                                                as "Leerfeld2"
+                                , NULL                                                as "Gemeindekennziffer"
+                                , 'Getränkefachgroßhandel'                            as "Geschäftstyp"
+                                , merchant.name                                       as "Name-1 (Bezeichnung)"
+                                , NULL                                                as "Name-2 (Inhaber)"
+                                , merchant.name                                       as "Kurzbezeichnung"
+                                , concat( address.street, ' ', address.house_number ) as "Straße u. Hausnummer"
+                                , zip                                                 as "Postleitzahl"
+                                , city                                                as "Ort"
+                                , 'Deutschland'                                       as "Land"
+                                , contact.mobile_phone                                as "Telefon-1"
+                                , NULL                                                as "Telefon-2"
+                                , NULL                                                as "Leerfeld2"
+                                , NULL                                                as "Telefax"
+                                , NULL                                                as "Leerfeld3"
+                                , to_char( now( )::date, 'YYYYMMDD' )                 as "Übertragungsdatum"
+                                , 'N'                                                 as "Status"
+                                , '10000004'                                          as "Übertragungsnummer"
+                            from fdw_customer_service.merchant
+                                    left join fdw_customer_service.merchant_has_contacts
+                                                on merchant.id_merchant = merchant_has_contacts.fk_merchant
+                                    left join fdw_customer_service.contact on merchant_has_contacts.fk_contact
+                                = contact.id_contact
+                                    left join fdw_customer_service.address on address.id_address = merchant.fk_address
+                            where merchant.name not like '%test%' and merchant.name not like '%Test%'
+                        )
 
 
-                )
+        , customers as (
+                            select '01'                                                as "Satzart"
+                                , '00000001'                                          as "Empfänger"
+                                , '43999023'                                          as "Absender"
+                                , '01'                                                as "Versionsnummer"
+                                , concat( 'O_', id_customer )                         as "Hersteller-Kunden-Nr."
+                                , NULL                                                as "GEDAT-Adressnummer"
+                                , NULL                                                as "GLN"
+                                , NULL                                                as "Leerfeld"
+                                , NULL                                                as "Leerfeld1"
+                                , NULL                                                as "Gemeindekennziffer"
+                                , NULL                                                as "Geschäftstyp"
+                                , customer.name                                       as "Name-1 (Bezeichnung)"
+                                , customer.name2                                      as "Name-2 (Inhaber)"
+                                , customer.short_name                                 as "Kurzbezeichnung"
+                                , concat( address.street, ' ', address.house_number ) as "Straße u. Hausnummer"
+                                , address.zip                                         as "Postleitzahl"
+                                , address.city                                        as "Ort"
+                                , address.country                                     as "Land"
+                                , contact.mobile_phone                                as "Telefon-1"
+                                , NULL                                                as "Telefon-2"
+                                , NULL                                                as "Leerfeld2"
+                                , NULL                                                as "Telefax"
+                                , NULL                                                as "Leerfeld3"
+                                , to_char( now( )::date, 'YYYYMMDD' )                 as "Übertragungsdatum"
+                                , 'N'                                                 as "Status"
+                                , '10000004'                                          as "Übertragungsnummer"
+                            from fdw_customer_service.customer
+                                    left join fdw_customer_service.address
+                                                on customer.fk_address = address.id_address
+                                    left join fdw_customer_service.customer_has_contacts
+                                                on customer.id_customer = customer_has_contacts.fk_customer
+                                    left join fdw_customer_service.contact
+                                                on customer_has_contacts.fk_contact = contact.id_contact
+                            where customer.name not like '%test%' and customer.name not like '%Test%'
+
+                        )
 
 
-    , final as (
+        , final as (
                         select *
                         from customers
 
@@ -112,9 +112,9 @@ def run_gedat():
                         from merchants
                     )
 
-    select *
-    from final
-    where "Straße u. Hausnummer" <> ''
+        select *
+        from final
+        where "Straße u. Hausnummer" <> ''
                                     """
 
     df = pd.read_sql_query(query, pg_engine)

7b0f609345d6f99994bce7d3d9a8e6f8ac426a47
Mohamed Ali <mohamedaligalall@gmail.com>
2022-08-25 11:08:12 +0200

    add running matching through airflow

---
+++ dags/include/dbt_run_matching.py
@@ -0,0 +1,22 @@
+
+
+# load_dotenv('enviroment_variables.env')
+
+def dbt_run_all_layers():
+    import os
+    import requests
+    import json
+    from airflow.models import Variable
+    # load_dotenv('enviroment_variables.env')
+
+    myToken = Variable.get("dbt_token")
+    myUrl =  Variable.get("Matching_url")
+
+    #string  = {'Authorization': 'token {}'.format(myToken),'cause' :'Kick Off From Testing Script'}
+    head ={'Authorization': 'token {}'.format(myToken)}
+    body ={'cause' :'Kicked Off From Airflow Diffs Pipeline'}
+    r = requests.post(myUrl, headers=head,data=body)
+    r_dictionary= r.json()
+    print(r.text)
+
+

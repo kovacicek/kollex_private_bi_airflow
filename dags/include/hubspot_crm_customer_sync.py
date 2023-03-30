05725ca0ea33c9115b56c658824da52545016b51
Milan Kovacic <milan.kovacic@pythondevcenter.com>
2023-03-30 09:32:21 +0200

    BI-83: appending new data, without clearing all sheet

--- dags/include/hubspot_crm_customer_sync.py
+++ dags/include/hubspot_crm_customer_sync.py
@@ -20,12 +20,53 @@ def hubspot_sync():
 
     sheet_name = Variable.get("HUBSPOT_SHEET_NAME")
     sql = """
-        SELECT * FROM prod_info_layer.customer_hubspot_upload
+       WITH hubspot AS (
+	SELECT 
+		*, 
+		CASE 
+			WHEN parent_customer_owner_lead_source LIKE '%Krombacher Brauerei%' THEN REPLACE(parent_customer_owner_lead_source, 'Krombacher Brauerei', 'Krombacher')
+			WHEN parent_customer_owner_lead_source LIKE '%krombacher%' THEN REPLACE(parent_customer_owner_lead_source, 'krombacher', 'Krombacher')
+			ELSE parent_customer_owner_lead_source
+		END AS parent_customer_owner_lead_source_updated
+	FROM prod_info_layer.customer_hubspot_upload
+)
+
+SELECT 
+	hub."customer_uuid",
+	hub."parent_customer_name",
+	hub."parent_customer_creation_date",
+	hub."parent_customer_owner_first_name",
+	hub."parent_customer_owner_last_name",
+	hub."parent_customer_owner_email",
+	hub."parent_customer_owner_mobile_phone",
+	hub."parent_customer_owner_lead_source_updated" AS "parent_customer_owner_lead_source",
+	hub."Wann JV-Typeform ausgefüllt",
+	hub."type",
+	hub."First Order Date",
+	hub."Child Customer Name",
+	hub."Customer Status",
+	hub."Number Of Orders",
+	hub."Status Last Order",
+	hub."Status Last Supplier Request",
+	hub."street",
+	hub."PLZ",
+	hub."Ort",
+	hub."Wann zuletzt bestellt",
+	hub."Gilt als Aktiv",
+	hub."Gilt als inaktiv (hat bereits bestellt, ist seit 28 inaktiv)",
+	hub."Registrierter Kunde ohne Bestellung",
+	hub."Wie oft durchschnittlichen in den letzten 3 Monaten bestellt",
+	hub."Durchschnittlicher Bestellrythmus (allgemein)",
+	hub."Wann eingeladen",
+	hub.merchant_who_invited_customer
+FROM hubspot as hub
     """
+    print("Reading sql.")
     df = pd.read_sql(
         sql,
         con=pg_engine,
     )
+    print("Data has been loaded.")
     df["Number Of Orders"] = df["Number Of Orders"].astype(str)
     df["Number Of Orders"] = df["Number Of Orders"].replace("nan", "0")
     gsheet_credentials = {
@@ -48,26 +89,20 @@ def hubspot_sync():
     sh = gc.open_by_url(Variable.get("HUBSPOT_SPREADSHEET"))
 
     ws = sh.worksheet(sheet_name)
-    # Get the existing data as a dataframe
-    # sheet_as_df = gd.get_as_dataframe(ws)
-    # # Append the new dataframe to the existing dataframe
-    # df.columns = [c.replace(" ", "_") for c in df.columns]
-    # sheet_as_df.columns = [c.replace(" ", "_") for c in sheet_as_df.columns]
-    #
-    # df["Customer_UUID"] = df["Customer_UUID"].astype(str)
-    # sheet_as_df["Customer_UUID"] = sheet_as_df["Customer_UUID"].astype(str)
-    #
-    # current_ids = sheet_as_df["Customer_UUID"].tolist()
-    # new_ids = df["Customer_UUID"].tolist()
-    # missing_ids = list(set(new_ids) - set(current_ids))
-    # filter_df = df[df["Customer_UUID"].isin(missing_ids)]
-    #
-    # df.columns = [c.replace("_", " ") for c in df.columns]
-    # sheet_as_df.columns = [c.replace("_", " ") for c in sheet_as_df.columns]
-    # filter_df.columns = [c.replace("_", " ") for c in filter_df.columns]
-    #
-    # appended_df = sheet_as_df.append(filter_df)
-    # Clear the existing sheet
-    ws.clear()
-    gd.set_with_dataframe(ws, df)
-    print("Dataframe has been appended to the sheet")
+
+    print("Get existing sheet.")
+    try:
+        existing_data = ws.get_all_records()
+        df_existing = pd.DataFrame(existing_data)
+    except gs.exceptions.APIError:
+        df_existing = pd.DataFrame()
+
+    if not df_existing.empty:
+        print("Comparing data.")
+        df_diff = df[~df["customer_uuid"].isin(df_existing["customer_uuid"])]
+    else:
+        df_diff = df
+
+    # Append new rows to worksheet
+    gd.set_with_dataframe(ws, df_diff, include_column_header=True, row=1)
+    print("Data has been appended to the sheet")

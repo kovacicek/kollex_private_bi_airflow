8e2222c5987df71aea81b1f7a1c341379b22634d
danilodobras <danilodobras97@gmail.com>
2023-04-24 09:57:32 +0200

    BI-194: change to FULL to UPSERT

--- dags/PIM_Pipeline.py
+++ dags/PIM_Pipeline.py
@@ -3,13 +3,12 @@ import pandas as pd
 from airflow import DAG
 from airflow.models import Variable
 
-# from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import BranchPythonOperator, PythonOperator
 
 from datetime import datetime, timedelta
-from sqlalchemy import create_engine
 
+from include.db import prepare_pg_connection
 from include.delta_load_all_skus import run_delta_load
 from include.full_load_all_skus import run_full_load
 from include.dbt_run_raw_layer import dbt_run_raw_layers
@@ -19,15 +18,8 @@ from include.my_sql_to_postgres import my_sql_to_postgres
 
 def branch_on():
     # create engine
-    pg_host = Variable.get("PG_HOST")
-    pg_user = Variable.get("PG_USERNAME_WRITE")
-    pg_password = Variable.get("PG_PASSWORD_WRITE")
-    pg_database = Variable.get("PG_DATABASE")
+    pg_engine = prepare_pg_connection()
     pg_schema = Variable.get("PG_RAW_SCHEMA")
-    pg_connect_string = (
-        f"postgresql://{pg_user}:{pg_password}@{pg_host}/{pg_database}"
-    )
-    pg_engine = create_engine(f"{pg_connect_string}", echo=False)
     # get merchants active
     merchants_active = pd.read_sql(
         """
@@ -134,7 +126,7 @@ with DAG(
             "mysql_tables_to_copy": "pim_catalog_product",
             "mysql_schema": "akeneo",
             "unique_column": "id",
-            "delta_load": "FULL_RELOAD",
+            "delta_load": "UPSERT",
             "timestamp_column": " updated",
             "look_back_period": 0,
             "chunksize_to_use": 2000,
@@ -151,7 +143,7 @@ with DAG(
             "mysql_schema": "akeneo",
             "timestamp_column": "updated",
             "unique_column": "id",
-            "delta_load": "FULL_RELOAD",
+            "delta_load": "UPSERT",
             "look_back_period": 0,
             "chunksize_to_use": 10000,
         },
@@ -167,7 +159,7 @@ with DAG(
             "timestamp_column": "updated_at",
             "mysql_schema": "gfghdata",
             "unique_column": "id",
-            "delta_load": "FULL_RELOAD",
+            "delta_load": "UPSERT",
             "look_back_period": 0,
             "chunksize_to_use": 10000,
         },

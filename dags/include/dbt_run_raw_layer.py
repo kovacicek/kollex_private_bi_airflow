84661dcdd91505b1193d76ec4bcee7d7cdcba739
Milan Kovacic <milan.kovacic@pythondevcenter.com>
2023-04-28 09:48:11 +0200

    improve logging for dbt, pep8

--- dags/include/dbt_run_raw_layer.py
+++ dags/include/dbt_run_raw_layer.py
@@ -1,24 +1,18 @@
+import requests
+from airflow.models import Variable
 
 
 def dbt_run_raw_layers():
-    import os
-    import requests
-    import json
-    # os.chdir('include')
-    from airflow.models import Variable
+    my_token = Variable.get("dbt_token")
+    my_url = Variable.get("PIM_url")
 
-    # load_dotenv('enviroment_variables.env')
-
-    myToken =Variable.get("dbt_token")
-    myUrl = Variable.get("PIM_url")
-
-    #string  = {'Authorization': 'token {}'.format(myToken),'cause' :'Kick Off From Testing Script'}
-    head ={'Authorization': 'token {}'.format(myToken)}
-    body ={'cause' :'Kicked Off From Airflow PIM Pipeline'}
-    r = requests.post(myUrl, headers=head,data=body)
-    r_dictionary= r.json()
+    head = {'Authorization': 'token {}'.format(my_token)}
+    body = {'cause': 'Kicked Off From Airflow PIM Pipeline'}
+    r = requests.post(my_url,
+                      headers=head,
+                      data=body
+                      )
+    r_dictionary = r.json()
+    print(r_dictionary)
+    print(r.status_code)
     print(r.text)
-
-
-
-    
